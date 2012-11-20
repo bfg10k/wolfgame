0a1b5fd08987cc086e361515fc839ebc4c701ecd
carherco <carherco@gmail.com>
2012-11-20 23:48:48 +0100

    más templates

---
+++ apps/killer/modules/juego/templates/objetivoPueblerino.php
@@ -0,0 +1,36 @@
+<div id="user"> 
+    <div class="btn-group">
+    <a class="btn btn-info" href="<?php echo url_for('juego/index'); ?>"><i class="icon-user icon-white"></i> <?php echo $nombre ?></a>
+    <a class="btn btn-info dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+    <ul class="dropdown-menu">
+    <li><a href="<?php echo url_for('juego/editar'); ?>"><i class="icon-pencil"></i> Editar perfil</a></li>
+    <li><a href="<?php echo url_for('juego/salir'); ?>"><i class="icon-ban-circle"></i> Salir</a></li>
+    <li class="divider"></li>
+    <li><a href="<?php echo url_for('juego/rellenarInforme'); ?>"><i class="i"></i> Certifica la muerte de tu objetivo</a></li>
+    </ul>
+    </div>
+
+</div>
+
+<div id="nav-user">
+    <ul class="nav nav-list">
+    <li class="active"><a href="<?php echo url_for('juego/index'); ?>"><i class="icon-home icon-white"></i> Inicio</a></li>
+    <li><a href="<?php echo url_for('juego/objetivo'); ?>"><i class="icon-screenshot"></i>Tu objetivo</a></li>
+    <li><a href="<?php echo url_for('juego/blog'); ?>"><i class="icon-eye-open"></i>INTEF News</a></li> 
+    </ul>
+</div>
+  
+
+<div id="content-objetivo" class="rounded-corners">
+  Rol: Pueblerino
+  
+  Objetivo: Descubrir y sacrificar al hombre-lobo
+  
+  Función: Cada nuevo día, mientras el hombre-lobo siga vivo, 
+  debéis sacrificar al habitante del pueblo que creáis que es el hombre lobo. 
+  La elección de la persona sacrificada será mediante el sistema de votaciones.
+  
+</div>
+
+  
+  
