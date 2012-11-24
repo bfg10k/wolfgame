cc9e739205102a1751b0eb84534a084a0dc4b7a4
carherco <carherco@gmail.com>
2012-11-24 15:00:34 +0100

    videncia

---
+++ apps/killer/modules/juego/templates/videnciaSuccess.php
@@ -0,0 +1,32 @@
+<div id="user"> 
+    <div class="btn-group">
+    <a class="btn btn-info" href="<?php echo url_for('juego/index'); ?>"><i class="icon-user icon-white"></i> <?php echo $nombre ?></a>
+    <a class="btn btn-info dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+    <ul class="dropdown-menu">
+    <li><a href="<?php echo url_for('juego/editar'); ?>"><i class="icon-pencil"></i> Editar perfil</a></li>
+    <li><a href="<?php echo url_for('juego/salir'); ?>"><i class="icon-ban-circle"></i> Salir</a></li>
+    </ul>
+    </div>
+
+</div>
+
+<div id="nav-user">
+    <ul class="nav nav-list">
+    <li><a href="<?php echo url_for('juego/index'); ?>"><i class="icon-home icon-white"></i> Inicio</a></li>
+    <li class="active"><a href="<?php echo url_for('juego/objetivo'); ?>"><i class="icon-screenshot"></i>Tu objetivo</a></li>
+    <li><a href="<?php echo url_for('juego/blog'); ?>"><i class="icon-eye-open"></i>INTEF News</a></li> 
+    </ul>
+</div>
+  
+
+<div id="content-objetivo" class="rounded-corners">
+  Este es el rol de <?php echo $victima ?>:
+  
+  
+  Memoriza bien esta información pues no volverá a aparecer en ningún sitio.
+  
+  
+</div>
+
+  
+  
