a848425f680b3bc8067dc340c8a572feaa25af46
carherco <carherco@gmail.com>
2012-11-25 12:27:27 +0100

    blog de visitas

--- apps/killer/modules/visitas/templates/blogSuccess.php
+++ apps/killer/modules/visitas/templates/blogSuccess.php
@@ -13,29 +13,11 @@
 
 <h1>Cuaderno de bitácora</h1>
 
-<?php foreach($sf_data->getRaw('noticias') as $noticia): ?>
+    <?php foreach($sf_data->getRaw('noticias') as $noticia): ?>
     <div class="span8">
-        <h1><?php echo $noticia->getTitulo(); ?></h1>
-        <?php $foto = $noticia->getFotoNoticia(); if(!empty($foto)):?><img src="<?php echo image_path('../uploads/news/'.$noticia->getFotoNoticia()); ?> " /><?php endif ?>
-        <p><?php echo $noticia->getNoticia(); ?></p>
-        <div>
-            <span class="badge badge-success">Publicado el <?php echo $noticia->getFecha('d-m-Y H:i'); ?></span>
-            <span class="badge badge-success badge-comentarios"><?php echo $noticia->countHlComentarioss(); ?> comentarios</span>
-        </div> 
-        <div id="div_comentarios" class="span4" style="float:right; text-align: right; display: none;">
-          <?php foreach($noticia->getHlComentarioss() as $comentario): ?>
-              <?php echo $comentario->getTexto(); ?>
-              <hr/>
-          <?php endforeach ?>
-          <div id="new_comment">
-            <form method="POST" action="<?php echo url_for('visitas/comentar')?>">
-              <input type="hidden" name="id_noticia" value="<?php echo $noticia->getId(); ?>" />
-              <textarea name="texto" class="input-xlarge"></textarea>
-              <button type="submit" class="btn btn-success">Comentar de forma anónima</button>
-            </form>
-          </div>
-        </div>
-        <hr class="span8" />
+        <span><?php echo $noticia->getTitulo(); ?></span>:
+        <span><?php echo $noticia->getNoticia(); ?></span> - 
+        <span><?php echo $noticia->getFecha('d-m-Y H:i'); ?></span>
     </div>
 <?php endforeach ?>
 
